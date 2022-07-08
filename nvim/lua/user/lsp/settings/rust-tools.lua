042d546df164e10bcf1d13b7b4cdddc8f5c55864
Marius Guggenmos <MariusGuggenmos@gmail.com>
2022-07-08 16:30:35 +0200

    Add autopairs to neovim config

--- nvim/lua/user/lsp/settings/rust-tools.lua
+++ nvim/lua/user/lsp/settings/rust-tools.lua
@@ -4,13 +4,23 @@ local function on_attach_wrapper(client, bufnr)
     original_on_attach(client, bufnr)
 
     local opts = { noremap = true, silent = true }
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>lh", "<cmd>lua require('rust-tools.inlay_hints').toggle_inlay_hints()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>le", "<cmd>lua require('rust-tools.expand_macro').expand_macro()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>lh",
+        "<cmd>lua require('rust-tools.inlay_hints').toggle_inlay_hints()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>le",
+        "<cmd>lua require('rust-tools.expand_macro').expand_macro()<CR>", opts)
 end
 
 return {
-  server = {
-    on_attach = on_attach_wrapper,
-    capabilities = require("user.lsp.handlers").capabilities,
-  }
+    server = {
+        on_attach = on_attach_wrapper,
+        capabilities = require("user.lsp.handlers").capabilities,
+        settings = {
+            -- doesn't seem to make range formatting work
+            ['rust-analyzer'] = {
+                rustfmt = {
+                    enableRangeFormatting = true,
+                }
+            }
+        }
+    }
 }
