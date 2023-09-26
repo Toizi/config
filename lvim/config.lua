5e752b539cd76b6afa6695280f0af2c62a1cd0c2
Toizi <empty>
2023-09-26 21:02:36 +0200

    lvim: improve telescope layout

--- lvim/config.lua
+++ lvim/config.lua
@@ -93,39 +93,77 @@ lvim.leader = "space"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 
+-- Use which-key to add extra bindings with the leader-key prefix
+lvim.builtin.which_key.vmappings["y"] = { "\"+y", "Copy to clipboard" }
+
 -- unmap default keymappings
 lvim.keys.insert_mode["jk"] = false
 lvim.keys.insert_mode["kj"] = false
 lvim.keys.insert_mode["jj"] = false
 lvim.keys.insert_mode["kk"] = false
 
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
-local _, actions = pcall(require, "telescope.actions")
-lvim.builtin.telescope.defaults.mappings = {
-  -- for input mode
-  i = {
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
-    ["<C-n>"] = actions.cycle_history_next,
-    ["<C-p>"] = actions.cycle_history_prev,
+-- Telescope config
+local actions = require("telescope.actions")
+-- see :h telescope.layout
+lvim.builtin.telescope.defaults.layout_config = {
+  -- prompt_position = "top",
+  height = 0.9,
+  width = 0.9,
+  bottom_pane = {
+    height = 25,
+    preview_cutoff = 100,
   },
-  -- for normal mode
-  n = {
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
+  center = {
+    height = 0.4,
+    preview_cutoff = 40,
+    width = 0.5,
+  },
+  cursor = {
+    preview_cutoff = 40,
+  },
+  horizontal = {
+    preview_cutoff = 100,
+    preview_width = 0.5,
+  },
+  vertical = {
+    preview_cutoff = 40,
+  },
+  flex = {
+    flip_columns = 150,
   },
 }
 
--- Use which-key to add extra bindings with the leader-key prefix
-lvim.builtin.which_key.vmappings["y"] = { "\"+y", "Copy to clipboard" }
+for key, _ in pairs(lvim.builtin.telescope.pickers) do
+  if key ~= "planets" then
+    lvim.builtin.telescope.pickers[key].previewer = nil
+    lvim.builtin.telescope.pickers[key].theme = nil
+    lvim.builtin.telescope.pickers[key].layout_strategy = nil
+  end
+end
+
+-- default is "smart" which often cuts off way too much of the path to distinguish
+-- files with the same name
+lvim.builtin.telescope.defaults.path_display = { "truncate" }
+-- flex means it will either use horizontal or vertical, depending on the window width
+lvim.builtin.telescope.defaults.layout_strategy = "flex"
+
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
+lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
+lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
+lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
+
+lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
+lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
+lvim.builtin.telescope.defaults.mappings.i["<esc>"] = actions.close
 
 -- Open references in telescope instead of the quickfix window
-lvim.lsp.buffer_mappings.normal_mode["gr"] = { "<cmd>Telescope lsp_references<cr>", "References (telescope)" }
+-- show_line only shows the filename and not the match since we always have a preview anyway
+lvim.lsp.buffer_mappings.normal_mode["gr"] = { ":lua require('telescope.builtin').lsp_references({show_line=false})<cr>", "References (telescope)" }
 lvim.lsp.buffer_mappings.normal_mode["gR"] = { vim.lsp.buf.references, "References (Quick fix)" }
 
 -- Quick access to search through open buffers
-lvim.builtin.which_key.mappings["o"] = { "<cmd>Telescope buffers initial_mode=insert previewer=false<cr>", "Find buffer" }
+lvim.builtin.which_key.mappings["o"] = { "<cmd>Telescope buffers initial_mode=insert previewer=false theme=dropdown<cr>", "Find buffer" }
 
 -- Show suggestions for folding
 lvim.builtin.which_key.setup.plugins.presets.z = true
