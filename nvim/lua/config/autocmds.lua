306b3593b4865b0871af18dacd8b5f6edabaedb8
Toizi <empty>
2024-08-19 20:49:41 +0200

    nvim: change inlay hint color

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = vim.api.nvim_create_augroup("Change_InlayHintColor", { clear = true}),
+  callback = function()
+    vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#a19c9c"})
+  end,
+  desc = "Change inlay hint color",
+})
